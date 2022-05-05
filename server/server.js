f3db1932373a430f078613aa1bf739f570554df4
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-05 11:47:00 +0200

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT= 8080;
+app.listen(PORT);
+
+console.log('server listening on ' + PORT);
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/getcode', (req,res) =>{
+    console.log('get code request');
+})
