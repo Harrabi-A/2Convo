18efe246b2ee836188ef5a1b2ce1ac85db177256
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-07 23:50:37 +0200

    Update Navbar.js

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMessage} from '@fortawesome/free-solid-svg-icons'
 
 
 function Navbar() {
     return(
         <div className='navbar'>
-            <h1>2Convo</h1> 
+            <ul>
+                <li><span className='txtLogo'>2Convo</span></li> 
+                <li><FontAwesomeIcon icon={faMessage} className='msgIcon'></FontAwesomeIcon></li>
+                
+            </ul>
         </div>  
     )
 }
