aa9aa1318fe0ec589be649e51e4dad3b8b86bae0
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-07 23:51:05 +0200

    Update RequestCode.js

--- client/src/Components/RequestCode.js
+++ client/src/Components/RequestCode.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Text from './Text';
 
-function RequestCode () {
+const RequestCode = () => {
+    const [code, setCode] = useState('');
+/*
+    const GetCode = () => {
+        fetch("http://127.0.0.1:8080/getcode")
+        .then(res => {code= res.text()})
+    }
+*/
+    const handleGetCode = () => {
+        // create a new XMLHttpRequest
+        var xhr = new XMLHttpRequest()
+
+        // get a callback when the server responds
+        xhr.addEventListener('load', () => {
+        // update the state of the component with the result here
+        console.log(xhr.responseText)
+        setCode(xhr.responseText)
+        })
+
+        // open the request with the verb and the url
+        xhr.open('GET', 'http://127.0.0.1:8080/getcode')
+        // send the request
+        xhr.send()
+    }
+    
     return(
         <div className='RequestCode' style={{display:'flex', alignItems:'center', justifyContent: 'space-around'}}>
-            <button className='btnRequestCode' onClick={getCode()}>Request Code</button>
-            <Text text='(Press the button to get code [available for 24 hr])'></Text>
+            <ul>
+                <li><button className='btnRequestCode' onClick={handleGetCode}>Request Code</button></li>
+                <li><Text text={code}></Text></li>
+                <li></li>
+                
+            </ul>
+            <span>(Press the button to get code [available for 24 hr])</span>
         </div>
     )
 } 
 
-function getCode(){
-    fetch("http://127.0.0.1:8080/getcode")
-}
 
 export default RequestCode;
