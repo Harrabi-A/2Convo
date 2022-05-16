fcae14ad8e9b63aa6cda67636be5774ba6432113
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-16 11:50:48 +0200

    Add files via upload

---
+++ client/src/Components/CodeBox/Code/CodeID/CodeID.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './CodeID.css'
+
+const CodeID = (props) =>{
+    return(
+        <>
+            <span className='codeID'>{props.codeID}</span>
+        </>
+    )
+}
+
+export default CodeID;
