8482fc18cd067fe7ffc1e6865241a5b97da0fe1a
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-16 11:42:10 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,13 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import RequestCode from './Components/RequestCode';
-import RequestConvo from './Components/RequestConvo'
-import ChatBox from './Components/ChatBox'
-import Text from './Components/Text'
-
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faMessage} from '@fortawesome/free-solid-svg-icons'
+import './App.css';
+import CodeBox from './Components/CodeBox/CodeBox';
+import ChatBox from './Components/ChatBox/ChatBox'
 
 function App() {
   return (
-    <>
-        <div className="App">
-        <Navbar></Navbar>    
-        <RequestCode></RequestCode>
-        <RequestConvo></RequestConvo>
-        
-        <Text text='ChatBox'></Text>
-        <FontAwesomeIcon icon={faMessage} className='msgIcon'></FontAwesomeIcon>    
-
-        <ChatBox></ChatBox>
-        </div> 
-
-        
-    </>
+    <div className="App">
+      <CodeBox/>
+      <ChatBox/>
+    </div>
   );
 }
 
