64075518e48112b4e69f8d31501192c9ad4a02cb
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-16 11:56:56 +0200

    Create EndConvo.js

---
+++ client/src/Components/CodeBox/EndConvo/EndConvo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EndConvo.css';
+
+const EndConvo = () =>{
+    return(
+        <div className='endConvo'>
+            <button className='btnClose'>Close Convo</button>
+        </div>
+    )
+}
+
+export default EndConvo;
