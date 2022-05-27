6b2b01845c55117d367374171f0a0525d5d3826e
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-27 23:43:49 +0200

    Update Code.js

--- client/src/Components/CodeBox/Code/Code.js
+++ client/src/Components/CodeBox/Code/Code.js
@@ -6,13 +6,31 @@ import {faCopy} from '@fortawesome/free-solid-svg-icons'
 
 import './Code.css';
 import CodeID from "./CodeID/CodeID";
+import { useSelector } from "react-redux";
+
+import { actionCreators } from "../../../State";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import io from "socket.io-client";
+
+/*const socket = io.connect("http://localhost:3030")
+socket.emit("init","ownPublicKey");*/
+
+
 
 const Code = () =>{
-    const [codeID, setCodeID] = useState('');
     const [copied, setCopied] = useState(false);
 
-    // get personal convo code from server and save it in State
-    useEffect(() => { 
+    const ownPublicKey = useSelector((value) => value.keys.ownPublicKey )
+
+    const socket = useSelector((value) => value.socket)
+    console.log(socket)
+
+    
+    // get personal convo code from server and save it in redux
+   /* useEffect(() => { 
+        /*
         // create a new XMLHttpRequest
         var xhr = new XMLHttpRequest()
 
@@ -22,12 +40,26 @@ const Code = () =>{
             setCodeID(xhr.responseText)
         })
 
+        
         // open the request with the verb and the url
         xhr.open('GET', 'http://127.0.0.1:8080/getcode')
         // send the request
-        xhr.send()
-    }, []);
+        xhr.send("prova")*/
+
+        
+        
+
+    //}, [])
     
+
+    /*console.log(ownPublicKey)*/
+    //const codeID = useSelector((value) => value.codeID)
+    /*console.log(code)*/
+    const dispatch = useDispatch();
+    const {setCodeID} = bindActionCreators(actionCreators, dispatch);
+    //setCodeID("1234")
+    const codeID = useSelector((value) => value.codeID)
+
     return(
         <div className="code">
             <span className="txtInfoCode">Your Convo code:</span>
