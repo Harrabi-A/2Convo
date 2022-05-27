84cdd678278fe47daedb6cc498d36bcfca59b479
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-05-27 23:40:43 +0200

    Update index.js

--- client/src/State/Reducer/index.js
+++ client/src/State/Reducer/index.js
@@ -1,10 +1,16 @@
 import {combineReducers} from "redux";
+import CodeIDState from "./CodeIDState";
 import ConvoStateReducer from "./ConvoStateReducer";
+import Keys from "./Keys";
 import WaitingStateReducer from "./WaitingStateReducer";
+import Socket from "./Socket";
 
 const reducers = combineReducers({
     convoState: ConvoStateReducer,
-    waitingState: WaitingStateReducer
+    waitingState: WaitingStateReducer,
+    keys: Keys,
+    codeID: CodeIDState,
+    socket: Socket
 })
 
 export default reducers;
