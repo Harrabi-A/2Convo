df9d551e48c46919139c34f38e32ba27b651fe18
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-07-02 18:36:45 +0200

    Update EndConvo.js

--- client/src/Components/CodeBox/EndConvo/EndConvo.js
+++ client/src/Components/CodeBox/EndConvo/EndConvo.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './EndConvo.css';
 
+import {useDispatch, useSelector} from "react-redux"
+import {bindActionCreators} from "redux"
+import { actionCreators } from '../../../State/index';
+
+
 const EndConvo = () =>{
+    const dispatch = useDispatch();
+    const {setConvoState, unsetConvoState} = bindActionCreators(actionCreators, dispatch);
+    const {setWaiting,unsetWaiting} = bindActionCreators(actionCreators, dispatch)
+
+    const waitingState = useSelector((state) => state.waitingState)
+    const convoState = useSelector((state) => state.convoState)
+    var btnText;
+    if(waitingState){
+        btnText="Stop request"
+    }
+    if(convoState){
+        btnText="End Convo"
+    }
+    
+    const handleClick = () =>{
+        if(waitingState){
+            unsetWaiting()
+        }
+        if(convoState){
+            unsetConvoState()
+        }
+    }
+
     return(
         <div className='endConvo'>
-            <button className='btnClose'>Close Convo</button>
+            {convoState || waitingState ? <button className='btnClose' onClick={handleClick}>{btnText}</button>:<></>}
         </div>
     )
 }
