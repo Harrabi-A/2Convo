ea6036d3bee5d090e64ee335887d793b447de0ae
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-07-03 21:38:07 +0200

    Update EndConvo.js

--- client/src/Components/CodeBox/EndConvo/EndConvo.js
+++ client/src/Components/CodeBox/EndConvo/EndConvo.js
@@ -4,15 +4,18 @@ import './EndConvo.css';
 import {useDispatch, useSelector} from "react-redux"
 import {bindActionCreators} from "redux"
 import { actionCreators } from '../../../State/index';
+import { act } from 'react-dom/test-utils';
 
 
 const EndConvo = () =>{
     const dispatch = useDispatch();
     const {setConvoState, unsetConvoState} = bindActionCreators(actionCreators, dispatch);
     const {setWaiting,unsetWaiting} = bindActionCreators(actionCreators, dispatch)
+    const {deleteAllMessages} = bindActionCreators(actionCreators, dispatch)
 
     const waitingState = useSelector((state) => state.waitingState)
     const convoState = useSelector((state) => state.convoState)
+    const socket = useSelector((value) => value.socket)
     var btnText;
     if(waitingState){
         btnText="Stop request"
@@ -24,9 +27,12 @@ const EndConvo = () =>{
     const handleClick = () =>{
         if(waitingState){
             unsetWaiting()
+            socket.emit("endWaiting")
         }
         if(convoState){
             unsetConvoState()
+            deleteAllMessages()
+            socket.emit("endConvo")
         }
     }
 
